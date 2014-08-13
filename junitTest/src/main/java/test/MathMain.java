56aec52f9ee29dcf85397864ee8ccd88c1a431af
michbudz <Praktyk2@10.1.1.236>
2014-08-13 12:59:48 +0200

    test 4

---
+++ junitTest/src/main/java/test/MathMain.java
@@ -0,0 +1,11 @@
+package test;
+
+public class MathMain {
+
+	public static void main(String[] args) {
+		Math m = new Math();
+		System.out.println(m.divide(100.0, 6.0));
+		System.out.println(m.multiple(100.0, 6.0));
+	}
+
+}
---
+++ junitTest/src/test/java/test/MathTest.java
@@ -0,0 +1,27 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MathTest {
+
+	@Test
+	public void testMath() {
+		Math m = new Math();
+	}
+
+	@Test
+	public void testMultiple() {
+		Math m = new Math();
+		m.multiple(0.0, 0.0);
+	}
+	
+	@Test
+	public void testDivide(){
+		Math m = new Math();
+		assertEquals((Double)1.1,m.divide(0.0, 0.0));
+
+	}
+
+}
